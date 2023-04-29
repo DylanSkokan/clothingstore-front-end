00b28a79cd352e392048a0e5919d27a8c31feb82
Tom Waterman <93015416+TomWaterdude@users.noreply.github.com>
2023-04-28 21:01:59 -0500

    Checkout
    Just about finished, a few things to tidy up but bringing the checkout page to main with the final layout

--- src/app/checkout-page/checkout-page.component.ts
+++ src/app/checkout-page/checkout-page.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
+import { Subject, takeUntil } from 'rxjs';
+import { Product } from '../model/product';
+
+interface Cart {
+  products: Product[];
+  totalCost: number;
+}
+
+
 
 @Component({
   selector: 'app-checkout-page',
   templateUrl: './checkout-page.component.html',
   styleUrls: ['./checkout-page.component.css']
 })
-export class CheckoutPageComponent {
+export class CheckoutPageComponent implements OnInit{
+  private unsubscribe = new Subject<void>();
+  cart: Cart = { products: [], totalCost: 0 };
+  public totalItems : number = 0;
+
+  constructor(private cartService : CartService){}
+
+  ngOnInit(): void {
+    this.cart = this.cartService.getCart()
+    this.cartService.getCartUpdated()
+    .pipe(takeUntil(this.unsubscribe))
+    .subscribe(() => {
+      this.totalItems = this.cartService.getCartSize();
+    });
+
+  }
 
 }
